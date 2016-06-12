ecacc2f35d70179f41edf323b2db6fa61db9e3a7
Arun Vasudevan <arunv83@gmail.com>
2016-06-12 12:30:07 -0700

    Add index.html and app.js

---
+++ client/app/app.js
@@ -0,0 +1,27 @@
+//app.js
+import React from 'react';
+import Router from 'react-router';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+    // ADD BINDING HERE
+  }
+
+  render() {
+    return (
+      // ADD STUFF HERE
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
