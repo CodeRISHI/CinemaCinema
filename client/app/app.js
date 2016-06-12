7c2850cee09f2301eb7e2aa3f268488bfeeb361e
Arun Vasudevan <arunv83@gmail.com>
2016-06-12 13:30:48 -0700

    Update components

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Router from 'react-router';
 import ReactDOM from 'react-dom';
+import Search from '../components/Search.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -16,9 +17,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <h1>{'Search For Movies, Actors, etc.'}</h1>
         <Search />
       </div>
-    )
+    );
   }
 }
 
